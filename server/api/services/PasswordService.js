29f7be566b170e947b6cf52af14ec59a16a6fc38
Thien Minh <nguyenleminhdev@gmail.com>
2020-01-05 18:11:30 +0700

    add docker

---
+++ server/api/services/PasswordService.js
@@ -0,0 +1,32 @@
+'use strict'
+const crypto = require('crypto')
+
+module.exports = passwordService = {
+    genSalt: (next) => {
+        crypto.randomBytes(
+            16,
+            (e, r) => (e) ? next(e) : next(null, r.toString('hex'))
+        )
+    },
+    genHash: (password, salt, next) => {
+        try {
+            crypto.pbkdf2(
+                password,
+                salt,
+                1000,
+                64,
+                'sha512',
+                (e, r) => (e) ? next(e) : next(null, r.toString('hex'))
+            )
+        } catch (e) {
+            next(e.message)
+        }
+    },
+    validate: (password, hash, salt, next) => {
+        passwordService.genHash(
+            password,
+            salt,
+            (e, r) => (e) ? next(e) : next(null ,hash === r)
+        )
+    }
+}
