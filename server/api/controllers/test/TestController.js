7b2513c248d7e1e889eba7aa447f833cd2e2c61c
jackhoang1 <jackhoang1995@gmail.com>
2020-10-12 08:30:10 +0700

    update

---
+++ server/api/controllers/test/TestController.js
@@ -0,0 +1,13 @@
+/**
+ * NameController
+ *
+ * @description :: Server-side actions for handling incoming requests.
+ * @help        :: See https://sailsjs.com/docs/concepts/actions
+ */
+
+module.exports = {
+    'ping': (req, res) => {
+        res.ok('pong')
+    }
+};
+
